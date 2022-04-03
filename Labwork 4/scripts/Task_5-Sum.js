80be9170cf55f2a766f3d046b9b88a4d7fd6dd43
Shtymak <vova.shtimak@gmail.com>
2022-04-03 23:54:01 +0300

    [FIX] Empty-NaN bug - Task5

--- Labwork 4/scripts/Task_5-Sum.js
+++ Labwork 4/scripts/Task_5-Sum.js
@@ -2,19 +2,23 @@ function Sum() {
     this.array = [];
     this.read = function read() {
         let input = prompt('Введіть число: ');
-        if (!input) {
+        if (!input || isNaN(+input)) {
             return 'Це виявилось не числом';
         }
         while (!isNaN(+input)) {
-            this.array.push(+input);
-            input = prompt('Введіть число: ');
             if (!input) {
                 return {
                     sum: this.array.reduce((item, total) => total + item, 0),
                     max: this.array.reduce((a, b) => Math.max(a, b)),
                 };
             }
+            this.array.push(+input);
+            input = prompt('Введіть число: ');
         }
+        return {
+            sum: this.array.reduce((item, total) => total + item, 0),
+            max: this.array.reduce((a, b) => Math.max(a, b)),
+        };
     };
 }
 
